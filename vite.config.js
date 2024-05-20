431fabeaa91a57bbe75d70367c56d127d9e8aa94
OmarT98 <omar.abdeen980@gmail.com>
2024-05-20 20:31:40 +0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -6,19 +6,19 @@ export default defineConfig({
   base: "/apple_website/",
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 1000,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            return id
-              .toString()
-              .split("node_modules/")[1]
-              .split("/")[0]
-              .toString();
-          }
-        },
-      },
-    },
+    chunkSizeWarningLimit: 10000000,
+    // rollupOptions: {
+    //   output: {
+    //     manualChunks(id) {
+    //       if (id.includes("node_modules")) {
+    //         return id
+    //           .toString()
+    //           .split("node_modules/")[1]
+    //           .split("/")[0]
+    //           .toString();
+    //       }
+    //   },
+    // },
+    // },
   },
 });
